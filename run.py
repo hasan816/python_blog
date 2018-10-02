e8b5428a89a2aff438a12d0f425fda81a2310e0e
bitpy86 <bitpy86@gmail.com>
2018-10-02 12:16:19 -0500

    Database created, app now a package

---
+++ run.py
@@ -0,0 +1,18 @@
+# -----------------------------------------------------------------------------------------
+#
+# Program:      My Python Blog
+#
+# Author:       Hasan Abdulhamid
+#
+# Description:  Personal blog about learning and building with Python.
+#               Features user registration, profiles and posting.
+#
+# File:         run.py - This is the main file for My Python Blog
+#
+# -----------------------------------------------------------------------------------------
+
+
+from pyblog import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
